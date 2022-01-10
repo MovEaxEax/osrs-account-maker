3feb3b1624ed707b250bd1eb993ffb46a035da14
Smug Pepe <59608685+MovEaxEax@users.noreply.github.com>
2022-01-10 21:28:56 +0100

    Add files via upload

---
+++ OSRS-Account-Creator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSRS_Account_Creator
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
